6deaaf5d176d30e0c1a55176b408e141b247fe77
Dávid Šmihula <david.smihula@student.tuke.sk>
2018-11-04 20:15:38 +0100

    Update Mjolnir.java

--- src/main/java/sk/tuke/kpi/oop/game/tools/Mjolnir.java
+++ src/main/java/sk/tuke/kpi/oop/game/tools/Mjolnir.java
@@ -2,25 +2,13 @@ package sk.tuke.kpi.oop.game.tools;
 
 import sk.tuke.kpi.gamelib.graphics.Animation;
 
-public class Mjolnir extends Hammer {
+public class Mjolnir extends BreakableTool {
 
-    private int uses;
-
-    public Mjolnir(){
-        uses=4;
-
+    public Mjolnir() {
+        super(4);
         setAnimation(new Animation("sprites/hammer.png", 16, 16));
     }
-
-    public void use (){
-        uses=uses-1;
-        if(uses==0){
-            getScene().removeActor(this);
-        }
-
-    }
-
-    public int getUses() {
-        return uses;
-    }
 }
+
+
+
