85115b99c308b530d89fc5ffa4345a7123a39c3c
Dávid Šmihula <david.smihula@student.tuke.sk>
2018-11-04 18:03:43 +0100

    Update Wrench.java

--- src/main/java/sk/tuke/kpi/oop/game/tools/Wrench.java
+++ src/main/java/sk/tuke/kpi/oop/game/tools/Wrench.java
@@ -1,36 +1,7 @@
 package sk.tuke.kpi.oop.game.tools;
 
-import sk.tuke.kpi.gamelib.graphics.Animation;
-
-public class Wrench extends BreakableTool {
-
-    private int remainingUses;
-
-
-    private int uses;
-
-    public Wrench(){
-        uses=2;
-
-        setAnimation(new Animation("sprites/wrench.png", 16, 16));
-    }
-
-    public void use (){
-        uses=uses-1;
-        if(uses==0){
-            getScene().removeActor(this);
-        }
-
-    }
-
-    public int getUses() {
-        return uses;
-    }
-
-
-
-
-
+public class Wrench  {
 
 
 }
+
